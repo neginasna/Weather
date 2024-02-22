36603bbd8346d46cdcb47220a5075c09fc3f3c8b
Negin <asnavandinegin@gmail.com>
2024-02-23 01:58:42 +0330

    api

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 
-// const INFOS = {
-//     BASE_URL: 'http://api.weatherapi.com/v1/current.json',
-//     API_KEY: '8254ac8caaf2433bbf1155457240602',
+// const infos = {
+//     apiUrl: 'http://api.weatherapi.com/v1/current.json',
+//     apiKey: '8254ac8caaf2433bbf1155457240602',
 // }
 
-// const inp = document.querySelector('input');
-
-// inp.addEventListener('keyup', eventHandler);
-
-// let showWeather = () => {
-//     getWeather(inp.value);
-// }
-
-// function getWeather(city) {
-//     fetch(`${INFOS.BASE_URL}?key=${INFOS.API_KEY}&q=${city}`)
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-// }
-
-
-// function eventHandler(e) {
-//     if (e.key === 'Enter') {
-//         showWeather();
-//     }
-// }
-
-
+const apiKey ="76bcab4ba4a0893b31771ad4ff501843";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric";
