5c7b12f14617fd77a252bd3512ae526aab7b29de
Negin <asnavandinegin@gmail.com>
2024-02-22 22:01:04 +0330

    input style

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// const INFOS = {
+//     BASE_URL: 'http://api.weatherapi.com/v1/current.json',
+//     API_KEY: '8254ac8caaf2433bbf1155457240602',
+// }
+
+// const inp = document.querySelector('input');
+
+// inp.addEventListener('keyup', eventHandler);
+
+// let showWeather = () => {
+//     getWeather(inp.value);
+// }
+
+// function getWeather(city) {
+//     fetch(`${INFOS.BASE_URL}?key=${INFOS.API_KEY}&q=${city}`)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// }
+
+
+// function eventHandler(e) {
+//     if (e.key === 'Enter') {
+//         showWeather();
+//     }
+// }
+
+
