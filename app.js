109d6b9fcdb51695d52d9c7f304909b9ee5973f3
Negin <asnavandinegin@gmail.com>
2024-02-23 13:41:44 +0330

    weather display block

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 
-// const infos = {
-//     apiUrl: 'http://api.weatherapi.com/v1/current.json',
-//     apiKey: '8254ac8caaf2433bbf1155457240602',
-// }
-
 const apiKey ="76bcab4ba4a0893b31771ad4ff501843";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const inp = document.querySelector("input");
 const btn = document.querySelector("button");
-
+const weatherIcone = document.querySelector(".weather-icone");
 
 
 async function getWeather(city){
 
     const response = await fetch (apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
+    let data = await response.json();
 
-    console.log(data);
-
-    const loc = document.querySelector(".city");
-    loc.innerHTML = data.name;
+    const location = document.querySelector(".city");
+    location.innerHTML = data.name;
 
     const temperature = document.querySelector(".temp");
     temperature.innerHTML = Math.round(data.main.temp) + "°c";
@@ -30,6 +23,31 @@ async function getWeather(city){
 
     const wind = document.querySelector(".wind");
     wind.innerHTML = data.wind.speed + "km/h";
+
+
+    if(data.weather[0].main == "Clouds"){
+      
+       weatherIcone.src = `images/clouds.png`
+    } 
+    else if (data.weather[0].main == "Clear") {
+        weatherIcone.src = `images/clear.png`
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcone.src = `images/rain.png`
+    }
+    else if (data.weather[0].main == "Snow") {
+        weatherIcone.src = `images/snow.png`
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcone.src = `images/drizzle.png`
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcone.src = `images/mist.png`
+    }
+
+    const weather = document.querySelector(".weather")
+    weather.style.display = "block";
+
 }
 
 btn.addEventListener('click', () => {
@@ -37,3 +55,5 @@ btn.addEventListener('click', () => {
     getWeather(inp.value);
 
 })
+
+
